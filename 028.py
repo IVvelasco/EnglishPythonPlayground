19fb4f1101ab29663e51a5f2ff9fec8c78110f68
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-27 11:34:57 -0300

    Binary into Letter

---
+++ 028.py
@@ -0,0 +1,121 @@
+
+# Level 028
+
+#  ------------------------
+#  -- BINARY INTO LETTER --
+#  ------------------------
+
+# My attempt building a code that decodes binary
+
+
+
+key = input('Digite o que você quer traduzir para binário: ')
+retorno = ''
+for key in retorno:
+    if key == 'A':
+        binario = retorno + '01000001'
+    elif key == 'B':
+        retorno = retorno + '01000010'
+    elif key == 'C':
+        retorno = retorno + '01000011'
+    elif key == 'D':
+        retorno = retorno + '01000100'
+    elif key == 'E':
+        retorno = retorno + '01000101'
+    elif key == 'F':
+        retorno = retorno + '01000110'
+    elif key == 'G':
+        retorno = retorno + '01000111'
+    elif key == 'H':
+        retorno = retorno + '01001000'
+    elif key == 'I':
+        retorno = retorno + '01001001'
+    elif key == 'J':
+        retorno = retorno + '01001010'
+    elif key == 'K':
+        retorno = retorno + '01001011'
+    elif key == 'L':
+        retorno = retorno + '01001100'
+    elif key == 'M':
+        retorno = retorno + '01001101'
+    elif key == 'N':
+        retorno = retorno + '01001110'
+    elif key == 'O':
+        retorno = retorno + '01001111'
+    elif key == 'P':
+        retorno = retorno + '01010000'
+    elif key == 'Q':
+        retorno = retorno + '01010001'
+    elif key == 'R':
+        retorno = retorno + '01010010'
+    elif key == 'S':
+        retorno = retorno + '01010011'
+    elif key == 'T':
+        retorno = retorno + '01010100'
+    elif key == 'U':
+        retorno = retorno + '01010101'
+    elif key == 'V':
+        retorno = retorno + '01010110'
+    elif key == 'W':
+        retorno = retorno + '01010111'
+    elif key == 'X':
+        retorno = retorno + '01011000'
+    elif key == 'Y':
+        retorno = retorno + '01011001'
+    elif key == 'Z':
+        retorno = retorno + '01011010'
+    elif key == 'a':
+        retorno = retorno + '01100001'
+    elif key == 'b':
+        retorno = retorno + '01100010'
+    elif key == 'c':
+        retorno = retorno + '01100011'
+    elif key == 'd':
+        retorno = retorno + '01100100'
+    elif key == 'e':
+        retorno = retorno + '01100101'
+    elif key == 'f':
+        retorno = retorno + '01100110'
+    elif key == 'g':
+        retorno = retorno + '01100111'
+    elif key == 'h':
+        retorno = retorno + '01101000'
+    elif key == 'i':
+        retorno = retorno + '01101001'
+    elif key == 'j':
+        retorno = retorno + '01101010'
+    elif key == 'k':
+        retorno = retorno + '01101011'
+    elif key == 'l':
+        retorno = retorno + '01101100'
+    elif key == 'm':
+        retorno = retorno + '01101101'
+    elif key == 'n':
+        retorno = retorno + '01101110'
+    elif key == 'o':
+        retorno = retorno + '01101111'
+    elif key == 'p':
+        retorno = retorno + '01110000'
+    elif key == 'q':
+        retorno = retorno + '01110001'
+    elif key == 'r':
+        retorno = retorno + '01110010'
+    elif key == 's':
+        retorno = retorno + '01110011'
+    elif key == 't':
+        retorno = retorno + '01110100'
+    elif key == 'u':
+        retorno = retorno + '01110101'
+    elif key == 'v':
+        retorno = retorno + '01110110'
+    elif key == 'w':
+        retorno = retorno + '01110111'
+    elif key == 'x':
+        retorno = retorno + '01111000'
+    elif key == 'y':
+        retorno = retorno + '01111001'
+    elif key == 'z':
+        retorno = retorno + '01111010'
+    elif key == ' ':
+        retorno = retorno + '00100000'
+print('Sua saída: ' + retorno)
