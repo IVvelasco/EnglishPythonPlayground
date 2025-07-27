fae2f274ca4c444f37cc13ae7a0f56fc3349055d
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-27 11:33:49 -0300

    Silly Codes

---
+++ 026.py
@@ -0,0 +1,22 @@
+# Level 024
+
+# -----------------
+# -- SILLY CODES --
+# -----------------
+
+# Codes for flert ;)
+
+tentativa = input("Are u mine? ? [y/n]: ")
+if tentativa == 'y':
+    print("  000       000    ")
+    print(" 00000     00000   ")
+    print("00000000000000000  ")
+    print(" 000000000000000   ")
+    print("  0000000000000    ")
+    print("    000000000      ")
+    print("      00000        ")
+    print("       000         ")
+    print("        0          ")
+
+else:
+    print("I wasen't talking to you")
