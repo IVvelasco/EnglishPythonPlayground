a9e20f8dd683d79de1d5ddfce47701e45a95be6d
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-27 11:29:20 -0300

    Playing with Strings

---
+++ 024.py
@@ -0,0 +1,11 @@
+# Level 024
+
+# --------------------------
+# -- PLAYING WITH STRINGS --
+# --------------------------
+
+prase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+reversing = prase[::-1]
+print("")
+print(reversing)
+print("")
