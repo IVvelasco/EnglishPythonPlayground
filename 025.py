5a5770dab7b5afdbc3074b62c882110b3f0dab36
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-27 11:32:42 -0300

    Playing with Strings 2

---
+++ 025.py
@@ -0,0 +1,11 @@
+# Level 025
+
+# ----------------------------
+# -- PLAYING WITH STRINGS 2 --
+# ----------------------------
+
+phrase = input("Insert here the phrase you want to invert: ")
+reverse = phrase[::-1]
+print(f"The phrase is: {phrase}")
+print(f"Reverse: {reverse}")
+
