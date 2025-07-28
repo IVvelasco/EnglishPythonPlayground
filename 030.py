89799e995c14cd5b486bc2191ba01cf2d9b76c96
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-28 04:16:30 -0300

    Nightclub for over 18s

---
+++ 030.py
@@ -0,0 +1,14 @@
+# Level 030
+
+#  ----------------------------
+#  -- NIGHTCLUB FOR OVER 18S --
+#  ----------------------------
+
+# Following the law in Brazil, where u can go to nightclubs if your age is 18 or more.
+
+
+age = int(input("Insert your age here: "))
+if age >= 18:
+    print("You can go inside")
+else:
+    print("You need have at least 18s for be able to get inside this place.")
