0276906c3b9184ebbed6f991f53e4ad0efad3583
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-28 04:24:58 -0300

    Breaking the looping with the break function

---
+++ 036.py
@@ -0,0 +1,9 @@
+# Level 036
+# -----------------------------------------------
+# -- BREAKING THE LOOPING USING BREAK FUNCTION --
+# -----------------------------------------------
+
+rich = False
+while rich != True:
+    print("VAI TRABALHAR")
+    break # This will stop the looping, no matter what ;) NOW
