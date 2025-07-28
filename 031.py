e294a47fd85ff5a7080f0c93d5001f46fcb02303
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-28 04:18:00 -0300

    Chicken or Egg Quiz

---
+++ 031.py
@@ -0,0 +1,19 @@
+#Level 031
+
+# -------------------------
+# -- CHICKEN OR EGG QUIZ --
+# -------------------------
+
+print("Which came first: the chicken or the egg?")
+print("a) Chicken")
+print("b) Egg")
+answer = input("Your answer: ")
+if answer == 'a':
+    print("And the dinosaurs, huh?")
+elif answer == 'b':
+    print("Congratulations!")
+else:
+    print("Please enter a valid answer")
+     
+
+    
