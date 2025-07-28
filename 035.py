a5050feebe7a414ca1885f97b5baff30f797e170
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-28 04:22:37 -0300

    Letting the loop breaks your terminal

---
+++ 035.py
@@ -0,0 +1,11 @@
+# Level 035
+# ---------------------------------------------
+# -- LETTING THE LOOPING BREAK YOUR TERMINAL --
+# ---------------------------------------------
+
+rich = False
+while rich != True:
+    print("GO TO WORK")
+    
+# To stop printing, either change 'False' to 'True' on line 6 or put a break below print or delete the code and press control S
+    
