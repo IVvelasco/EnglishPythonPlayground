b02077841776a519eaae3ee933535bdfd67e3f89
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-08-14 14:46:15 -0300

    Grouping Students

---
+++ 044.py
@@ -0,0 +1,13 @@
+# Level 044
+
+#  --------------
+#  -- SILLY.py -- 
+#  --------------
+
+question = input("Qual é a melhor linguagem de programação?")
+if question == 'Python':
+    print("That's right!")
+elif question == 'python':
+    print("Yeahhh")
+else:
+    print("Nah, try again.")
