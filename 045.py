b5d2a287a4ca1796ecc8f7fac6681ad0688bd5ff
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-08-14 14:47:11 -0300

    Testing Ternary Operators - User Login or Not

---
+++ 045.py
@@ -0,0 +1,16 @@
+# Level 045
+
+# --------------------------------
+# -- TESTING TERNARY OPERATORS --
+# --     USER LOGIN OR NOT     --
+# -------------------------------
+
+def main():
+
+    user_login = True 
+    if user_login == True:
+        print("You have been successfully logged in!")
+    else:
+        print("Incorrect username or password, please try again!")
+if __name__ == "__main__":
+    main()
