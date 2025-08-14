b4940dff67d0626ffc46691d8f6bc5181d3e565c
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-08-14 14:43:45 -0300

    Heads or Tails

---
+++ 042.py
@@ -0,0 +1,16 @@
+# Level 042
+
+#  --------------------
+#  -- HEADS OR TAILS --
+#  --------------------
+
+# You don't know what to do? Play this virtual coin and discover!
+
+import random
+def main():
+
+    coin = ["Head", "Tail"]
+    print(random.choice(coin))
+
+if __name__ == "__main__":
+    main()
