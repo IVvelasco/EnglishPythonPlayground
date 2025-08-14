7cc02fd248690ab680330aed60e11cc3090db2dd
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-08-14 14:51:04 -0300

    CPF & Booleano

---
+++ 049.py
@@ -0,0 +1,13 @@
+# Level 049
+
+# --------------------
+# -- CPF & BOOLEANO --
+# --------------------
+
+# CPF is a brazilian documentation, a ID document
+
+cpf = True
+if cpf == True:
+    print("Your CPF is negative")
+else: 
+    print("Nothen to see here")
