adddeeec2dbffd12579dbe6702d41c02a2407e4e
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-08-14 14:49:10 -0300

    Drawing on the Terminal

---
+++ 047.py
@@ -0,0 +1,16 @@
+# Level 047
+
+# -----------------------------
+# -- DRAWING ON THE TERMINAL --
+# -----------------------------
+
+# Using print line by line, even with punctuation to replace spaces, makes everything look jumbled.
+# The solution was to turn each line into a single printout and use periods as spaces just to be safe.
+# But tests show that spaces are respected when printing line by line.
+
+print("#####..........#....#....####....#.......###......###......###......####.")
+print("#..............#....#....#.......#......#...#.....#.......#...#....#....#") 
+print("####...........#....#....###.....#......#####.......#.....#........#....#") 
+print("#..............#....#....#.......#......#...#....#...#....#........#....#")
+print("#####....#.......##......####....####...#...#.....####.....###......####.")
+
