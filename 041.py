7df03f9a3896a12519decd8ee4d666821c477a5a
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-08-14 14:39:12 -0300

    Declaration in infinite looping

---
+++ 041.py
@@ -0,0 +1,14 @@
+# Level 041
+
+#  -------------------------------------
+#  -- DECLARATION IN INFINITE LOOPING --
+#  -------------------------------------
+
+# This code send infinite kisses on your terminal 
+
+name = str(input("Input here the name of your your love: "))
+love = True 
+while love == True:
+    print(f"{name}: Kisses")
+else:
+    print("Run")
